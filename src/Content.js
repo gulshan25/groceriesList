455837f46526923947d636d1427b71e70bc8b5a7
gulshan25 <g.r.tanny@gmail.com>
2022-11-23 18:48:37 +0600

    ReactApp

--- src/Content.js
+++ src/Content.js
@@ -23,34 +23,60 @@ const Content = () => {
         }
     ]);
 
+    const handleCheck = (id) => {
+        const listItems = items.map((item) => item.id === id ? {...item, checked: !item.checked } : item);
+        setItems(listItems);
+        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    }
+
+    const handleDelete = (id) => {
+        const listItems = items.filter((item) => item.id !== id);
+        setItems(listItems);
+        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    }
+
 
     return ( <
 
-        main >
+        main > {
+            items.length ? ( <
+                ul > {
+                    items.map((item) => ( <
+                        li className = "item"
+                        key = { item.id } >
+                        <
+                        input type = "checkbox"
+                        onChange = {
+                            () => handleCheck(item.id)
+                        }
+                        checked = { item.checked }
+                        /> <
+                        label style = {
+                            (item.checked) ? { textDecoration: 'line-through' } : null
+                        }
+                        onDoubleClick = {
+                            () => handleCheck(item.id)
+                        } > { item.item } < /label>   <
+                        FaTrashAlt onClick = {
+                            () => handleDelete(item.id)
+                        }
+                        role = "button"
+                        tabIndex = "0" / >
+                        <
+                        /
+                        li >
+                    ))
+                } <
+                /ul>
+            ) : ( <
+                p style = {
+                    { marginTop: '2rem' }
+                } > Your list is empty < /p>
+            )
+        }
 
         <
-        ul > {
-            items.map((item) => ( <
-                li className = "item"
-                key = { item.id } >
-                <
-                input type = "checkbox"
-                checked = { item.checked }
-                /> <
-                label > { item.item } < /label>   <
-                FaTrashAlt role = "button"
-                tabIndex = "0" / >
-                <
-                /
-                li >
-            ))
-        } <
-        /ul>
-
-
-        <
-        /
-        main >
+        /main >
     )
 }
 
